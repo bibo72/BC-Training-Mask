43d8091c1fad76583ab53d3557a9016d19955bba
Charles <charles.yin@silksoftware.com>
2021-01-15 18:28:26 +0800

    Feature #117 Feature #111

--- assets/js/theme/Mask/optionModal.js
+++ assets/js/theme/Mask/optionModal.js
@@ -1,10 +1,11 @@
 export default function () {
     $('body').on('click', '[data-product-modal-trigger]', e => {
+        console.log('did');
         e.stopPropagation();
 
         const $currentTarget = $(e.currentTarget);
 
-        $currentTarget.next('.modal_content').toggle();
+        $currentTarget.next('.modal_content').show();
     }).on('click', '[data-product-modal-close]', e => {
         e.stopPropagation();
 
